3c0fbbb7cc1b3171ae1aadee162b915c42e62d00
L.B <labinotb@outlook.com>
2022-09-05 10:38:39 +0200

    Fix bugs

--- New folder/utils/checkPermissions.js
+++
@@ -1,8 +0,0 @@
-import { UnAuthenticatedError } from "../errors/index.js";
-
-const checkPermissions = (user) => {
-  if (user.role === "admin") return;
-  throw new UnAuthenticatedError("Not authorized to access this route");
-};
-
-export default checkPermissions;
